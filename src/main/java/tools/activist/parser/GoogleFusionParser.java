1510ce709f05560086c775501e40e8e8a2f4a448
Chris Howe <cjhowe@dhcdata.com>
2018-12-21 14:19:17 -0600

    change dotenv

--- src/main/java/tools/activist/parser/GoogleFusionParser.java
+++ src/main/java/tools/activist/parser/GoogleFusionParser.java
@@ -88,8 +88,7 @@ public class GoogleFusionParser {
   }
 
   public static GeocodingResult[] getGeocode(String address) {
-    Dotenv dotenv = Dotenv.load();
-    String GEOCODE_API_KEY = dotenv.get("GEOCODE_API_KEY");
+    String GEOCODE_API_KEY = Dotenv.load().get("GEOCODE_API_KEY");
     try {
       return GeocodingApi.geocode(new GeoApiContext.Builder().apiKey(GEOCODE_API_KEY).build(), address).await();
     } catch (Exception e) {
