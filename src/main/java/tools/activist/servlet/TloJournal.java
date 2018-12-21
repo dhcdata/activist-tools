5e5d748172f3e5f13dd432f5303dc4643d766b1a
Chris Howe <cjhowe@dhcdata.com>
2018-12-21 12:48:36 -0600

    move envdot to correct root

--- src/main/java/tools/activist/servlet/TloJournal.java
+++ src/main/java/tools/activist/servlet/TloJournal.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.logging.Logger;
+
 import tools.activist.handler.JsonHandler;
 import tools.activist.model.floor.JournalEntry;
 import tools.activist.parser.TloJournalParser;
@@ -15,11 +19,14 @@ import tools.activist.parser.TloJournalParser;
 @SuppressWarnings("serial")
 @WebServlet(name = "TloJournal", value = "/journal")
 public class TloJournal extends HttpServlet {
+  private static final Logger log = Logger.getLogger(TloJournal.class.getName());
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
     // TODO: Make session a global property somewhere
-    String session = req.getParameter("session") == null ? req.getParameter("session") : "85R";
+    Dotenv dotenv = Dotenv.load();
+    String session = req.getParameter("session") != null ? req.getParameter("session") : dotenv.get("DEFAULT_SESSION");
+    log.info("Session: " + session);
     List<JournalEntry> out = TloJournalParser.getStuff(req.getParameter("chamber"), req.getParameter("journalId"),
         session);
     JsonHandler.writeJsonToResponse(out, resp, out.getClass());
